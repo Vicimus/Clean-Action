82d6399cd0cce25a140b3814862a1717d5e6998c
Jordan Grieve <quintilex@gmail.com>
2021-04-14 09:28:23 -0400

    build

--- src/state.ts
+++ src/state.ts
@@ -1,1 +1,7 @@
+import * as coreCommand from '@actions/core/lib/command'
+
 export const isPost = !!process.env['STATE_isPost'];
+
+if (!isPost) {
+    coreCommand.issueCommand('save-state', { name: 'isPost' }, 'true')
+}
