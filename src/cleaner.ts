7d28c2f1dcef4905bf06b0cb3fb050da176cef78
Jordan Grieve <quintilex@gmail.com>
2021-04-14 09:09:25 -0400

    changes

--- src/cleaner.ts
+++ src/cleaner.ts
@@ -1,7 +1,11 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as fs from 'fs';
 
 export class Cleaner {
-    public clean(): void {
-        core.debug('Cleaning up real nice');
+    public async clean(): Promise<void> {
+        core.info('Going to remove some files brah');
+        core.info(github.context['workspace']);
+        core.info(JSON.stringify(process.env));
     }
 }
